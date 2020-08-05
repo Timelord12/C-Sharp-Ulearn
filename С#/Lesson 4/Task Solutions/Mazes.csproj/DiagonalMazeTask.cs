939b5c09141f520436f3d2b8dea7958ac4f4a432
Timelord12 <crossfirezharov@gmail.com>
2020-08-05 23:06:41 +0500

    Added Leson 4

---
+++ С#/Lesson 4/Task Solutions/Mazes.csproj/DiagonalMazeTask.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Mazes
+{
+	public static class DiagonalMazeTask
+	{
+		public static void MoveOut(Robot robot, int width, int height)
+		{
+			int longSide = Math.Max(width, height) - 2;
+			int shortSide = Math.Min(width, height) - 2;
+			Direction dirLong = (longSide + 2 == width) ? Direction.Right : Direction.Down;
+			Direction dirShort = (longSide + 2 == width) ? Direction.Down : Direction.Right;
+			int longOffset = longSide / shortSide;
+			DiagonalSteps(robot, dirLong, dirShort, shortSide, longOffset);
+		}
+
+		public static void MoveToN(Robot robot, Direction dir, int times)
+        {
+			for (int i = 0; i < times; i++)
+            {
+				robot.MoveTo(dir);
+            }
+        }
+
+		public static void DiagonalSteps(Robot robot, Direction dirLong, Direction dirShort, int shortSide, int longOffset)
+        {
+			for (int i = 0; i < shortSide; i++)
+			{
+				MoveToN(robot, dirLong, longOffset);
+
+				if (!robot.Finished)
+					robot.MoveTo(dirShort);
+			}
+		}
+
+		
+	}
+}
---
+++ С#/Lesson 4/Task Solutions/Mazes.csproj/EmptyMazeTask.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.ExceptionServices;
+
+namespace Mazes
+{
+	public static class EmptyMazeTask
+	{
+		public static void MoveOut(Robot robot, int width, int height)
+		{
+			width -= 3;
+			height -= 3;
+			for (int i = 0; i < (width + height); i++)
+				StepExecution(robot, width, i);
+		}
+
+		static void StepExecution(Robot robot, int width, int step)
+		{
+			if ((step - width) < 0)
+				robot.MoveTo(Direction.Right);
+			else
+				robot.MoveTo(Direction.Down);
+		}
+	}
+}
