c09e6a4ea3061908d4a96ca5abcae74816751919
Timelord12 <crossfirezharov@gmail.com>
2020-08-06 01:45:32 +0500

    Added Lesson 5

---
+++ С#/Lesson 5/Task Solutions/Names.csproj/HistogramTask.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace Names
+{
+    internal static class HistogramTask
+    {
+        public static HistogramData GetBirthsPerDayHistogram(NameData[] names, string name)
+        {
+            var dayNames = InitMonthDays();
+            var sumAtDay = new double[31];
+
+            foreach(var str in names)
+            {
+                sumAtDay[str.BirthDate.Day - 1] += (str.Name == name) ? 1 : 0;
+            }
+
+            sumAtDay[0] = 0;
+
+            return new HistogramData(
+                string.Format("Рождаемость людей с именем '{0}'", name), dayNames, sumAtDay);
+        }
+
+        static string[] InitMonthDays()
+        {
+            var dayNames = new string[31];
+
+            for (var i = 0; i < 31; i++)
+            {
+                dayNames[i] = Convert.ToString(i + 1);
+            }
+
+            return dayNames;
+        }
+    }
+}
